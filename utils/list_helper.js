f6161b82b944a72acbc1a90dac319895b87fef59
TimaGribanov <48593815+TimaGribanov@users.noreply.github.com>
2023-10-02 12:47:39 +0200

    4.7 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = blogs => {
   return 1
 }
@@ -12,7 +14,50 @@ const totalLikes = blogs => {
   return sum
 }
 
+const favouriteBlog = blogs => {
+  let max = 0
+
+  blogs.forEach(el => {
+    if (el.likes > max) max = el.likes
+  })
+
+  let favBlog
+
+  blogs.forEach(b => {
+    if (b.likes === max) favBlog = b
+  })
+
+  return favBlog
+}
+
+const mostBlogs = blogs => {
+  const mostAuthor = _(blogs).countBy('author').entries().maxBy(_.last)
+  return { author: mostAuthor[0], blogs: mostAuthor[1] }
+}
+
+const mostLikes = blogs => {
+  let likes = []
+  _.forEach(blogs, el => {
+    if (likes.some(e => e.author === el.author)) {
+      const index = likes.findIndex(obj => obj.author === el.author)
+      likes[index].likes += el.likes
+    }
+    else
+      likes.push({ author: el.author, likes: el.likes })
+  })
+
+  const orderedLikes = _.orderBy(likes, 'likes', 'desc')
+
+  return {
+    author: orderedLikes[0].author,
+    likes: orderedLikes[0].likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog,
+  mostBlogs,
+  mostLikes
 }
